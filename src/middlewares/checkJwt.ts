c4ebbbb7afeb9a42966371a964ccb0c00ce0c734
Edidiong <ephraimedy@gmail.com>
2025-06-06 20:59:08 +0100

    commit

---
+++ src/middlewares/checkJwt.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from "express";
+import { TokenExpiredError } from "jsonwebtoken";
+import { verify, JwtPayload } from "jsonwebtoken";
+import { UnauthorizedError } from "../exceptions";
+import { User } from "@prisma/client";
+import { prismaClient } from "../utils/prisma";
+import {
+    extractTokenFromHeaders,
+    verifyToken,
+    AuthTokenPayload
+} from "../utils/jwt";
+
+// CustomRequest interface to provide JWTs to controllers
+export interface CustomRequest extends Request {
+    token?: AuthTokenPayload;
+    user?: User;
+}
+
+export const checkJwt = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        // Get the JWT from the request header.
+        const token = extractTokenFromHeaders(req);
+        if (!token) {
+            throw new UnauthorizedError("No token provided");
+        }
+
+        let payload;
+        try {
+            payload = await verifyToken(token);
+        } catch (error: any) {
+            if (error instanceof TokenExpiredError) {
+                throw new UnauthorizedError("Token expired");
+            }
+            throw new UnauthorizedError("Invalid token");
+        }
+
+        // Check if the token has the required properties
+        if (!payload.id) {
+            throw new UnauthorizedError("Malformed token");
+        }
+
+        // Fetch the user from the database
+        const user = await prismaClient.user.findUnique({
+            where: {
+                id: payload.id as string,
+            },
+        });
+
+        if (!user) {
+            throw new UnauthorizedError("User not found");
+        }
+
+        // Explicitly cast req to CustomRequest and assign values
+        (req as CustomRequest).user = user;
+        (req as CustomRequest).token = payload;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
